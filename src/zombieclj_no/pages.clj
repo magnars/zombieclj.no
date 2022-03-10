141bf458ce562ddd1f2e3dc43eb0d8101a85b4c5
Magnar Sveen <magnars@gmail.com>
2022-03-10 20:44:04 +0100

    Sesong 2 er på lufta etter 8 år

--- src/zombieclj_no/pages.clj
+++ src/zombieclj_no/pages.clj
@@ -10,27 +10,38 @@
    :jhannes "Johannes Brodwall"
    :cia-audience "120 publikummere på CiA"})
 
-(defn- render-episode [{:keys [number name guest upcoming] :as episode}]
+(defn- render-episode [{:keys [number name guest upcoming note] :as episode}]
   [:div.episode
    [:a (if upcoming
          {:class "faded"}
          {:href (episode-url episode)})
+    (when note
+      [:span.note note])
     (when guest
       [:span.note "Starring " (guests guest)])
     (when upcoming
       [:span.note "Kommer " upcoming])
     "Episode " number ": " name]])
 
-(defn- render-season [season]
+(defn render-season* [season]
   [:div.season
    [:h2 (:name season)]
    [:p (:description season)]
    (map render-episode (:episodes season))])
 
+(defn- render-season [season]
+  (if (:old? season)
+    [:details.old-season
+     [:summary (:name season)]
+     (render-season* season)]
+    (render-season* season)))
+
 (defn index [content]
   {:body (-> (slurp (io/resource "index.html"))
              (str/replace "<episodes/>"
-                          (html (map render-season (:seasons content)))))})
+                          (html
+                           (map render-season (remove :old? (:seasons content)))
+                           (map render-season (filter :old? (:seasons content))))))})
 
 (defn- insert-disqus-thread [html episode]
   (-> html
@@ -53,12 +64,6 @@
                "."))]
       [:div.embed-container (embed-video episode)]
       [:ul.small.mbm.mts
-       (if (:vimeo episode)
-         [:li "Du kan " [:a {:href (str "https://vimeo.com/" (:vimeo episode))} "laste ned episoden på Vimeo"] "."]
-         [:li "Du kan også laste den ned og se på bussen: "
-          [:a {:href (str "http://dl.dropbox.com/u/3615058/" (:id settings) "/" filename "?dl=1")}
-           filename]
-          " (" (:size episode) ")"])
        (when-not next-episode
          [:li "Hvis du har gått glipp av noe, kan du sjekke ut "
           [:a {:href "/"} "alle episodene"]
